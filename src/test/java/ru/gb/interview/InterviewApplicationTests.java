18cd4175fce6bdb98647ff3e89de3548b5ee05f1
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2023-03-19 10:02:50 +0300

    Initial commit

---
+++ src/test/java/ru/gb/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
